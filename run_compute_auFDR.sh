5251737b03f9bc58eee485240ac0bd2a5806107a
rperi <rperi@usc.edu>
2022-03-20 00:40:41 -0700

    Update run_compute_auFDR.sh

--- run_compute_auFDR.sh
+++ run_compute_auFDR.sh
@@ -9,10 +9,9 @@ test_split=$2
 mode=$3
 
   python evaluate/evaluate_FDR.py \
-    --test_split ${test_split} \
     --mode ${mode} \
     --trials_root ${data_dir}/trials/${test_split} \
-    --data_root ${data_dir}/data/eval-${test_split} \
-    --pred_root ${data_dir}/transformed_embeddings/eval-${test_split}/${mode} \
-    --scores_root ${data_dir}/scores/eval-${test_split} \
+    --data_root ${data_dir}/data/${test_split} \
+    --pred_root ${data_dir}/transformed_embeddings/${test_split}/${mode} \
+    --scores_root ${data_dir}/scores/${test_split} \
     --eval_xvector
